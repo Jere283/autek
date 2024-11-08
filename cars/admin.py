45f9bfefde29086c495dc3f421f16efc81d3f6f2
Jere283 <jeremyfigueroa2015@outlook.com>
2024-11-08 00:04:41 -0600

    admin models

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CarModels, Cars, Colors, CarBrands
 
 # Register your models here.
+
+admin.site.register(Cars)
+admin.site.register(Colors)
+admin.site.register(CarBrands)
+admin.site.register(CarModels)
