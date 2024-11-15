a6013639bbb11b24bd6bcdd971c7da48b7c126a2
Jere283 <jeremyfigueroa2015@outlook.com>
2024-11-15 17:19:13 -0600

    cors

--- autek/settings.py
+++ autek/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     "debug_toolbar",
     'users',
     'cars',
@@ -61,6 +62,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -152,7 +154,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'https://frontent-autek.vercel.app'
+    'https://autek.onrender.com',
+    'https://frontent-autek.vercel.app',
 ]
 
 
